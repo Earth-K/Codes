fbbf6a5f8501738b2a0e9c0b90cb8f721f3b150e
earth-k <k94earth@naver.com>
2020-02-22 23:10:30 +0900

    산업 스파이의 편지 풀이 추가

--- ProblemSolving/BOJ/완전 탐색/산업 스파이의 편지/boj_3671.cpp
+++ ProblemSolving/BOJ/완전 탐색/산업 스파이의 편지/boj_3671.cpp
@@ -8,8 +8,8 @@ int N,answer;
 string num;
 bool usedNum[10000000];
 
-
 bool isPrimeNum(int n){
+    if(n==0 || n==1) return false;
     for(int i=2; i<=sqrt(n); i++){
         if(n%i==0)
             return false;
@@ -19,12 +19,12 @@ bool isPrimeNum(int n){
 int solve(string num){
     int ret = 0;
     sort(num.begin(), num.end());
-    //cout<<"sortedNum: "<<num<<endl;
     do{
         int n = atoi(num.c_str());
         if(usedNum[n])continue;
+        usedNum[n] = true;
         if(isPrimeNum(n)){
-            usedNum[n] = true;
+            //cout<<"Num: "<<n<<endl;
             ret += 1;
         }
     }while(next_permutation(num.begin(), num.end()));
@@ -52,6 +52,8 @@ int main(){
     for(int i=0; i<N; i++){
         cin>>num;
         answer = 0;
+        for(int j=0; j<10000000; j++)
+            usedNum[j] = false;
         for(int k=1; k<=num.size();k++){
             string madeNum;
             comb(0,0,k,madeNum);
