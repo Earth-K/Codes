364751bcd32b94230dcb7ca9fb0ba5e12b2d133d
earth-k <k94earth@naver.com>
2020-05-05 23:56:22 +0900

    N과 M (9) 풀이 추가

---
+++ ProblemSolving/BOJ/N과 M/N과 M (9)/boj_15663.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+set<string> s;
+bool idxCheck[9];
+
+void dfs(const vector<int>& numbers, int m, vector<int>& ans){
+    if(ans.size() == m){
+        string str;
+        for(int i=0; i<ans.size(); i++)
+            str += to_string(ans[i]);
+        if(s.find(str) != s.end()) return;
+
+        s.insert(str);
+        for(int num : ans)
+            cout<<num<<" ";
+        cout<<"\n";
+        return;
+    }
+
+    for(int i=0; i<numbers.size(); i++){
+        if(idxCheck[i]) continue;
+        
+        idxCheck[i] = true;
+        ans.push_back(numbers[i]);
+
+        dfs(numbers,m,ans);
+
+        idxCheck[i] = false;
+        ans.pop_back();
+    }
+}
+
+int main(){
+    int n,m;
+    vector<int> numbers, ans;
+    cin>>n>>m;
+    for(int i=0,num; i<n; i++){
+        cin>>num;
+        numbers.push_back(num);
+    }
+    sort(numbers.begin(), numbers.end());
+    dfs(numbers,m,ans);
+    return 0;
+}
