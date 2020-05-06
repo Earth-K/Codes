fb1f9e2a6bce053c7a2ffb5944ae531212a05b9f
earth-k <k94earth@naver.com>
2020-05-06 17:07:12 +0900

    N과 M (10) 풀이 개선

--- ProblemSolving/BOJ/N과 M/N과 M (10)/boj_15664.cpp
+++ ProblemSolving/BOJ/N과 M/N과 M (10)/boj_15664.cpp
@@ -5,33 +5,23 @@
 #include<set>
 using namespace std;
 
-set<string> s;
-bool idxCheck[9];
+bool check[9];
+set<vector<int>> s;
 
-void dfs(const vector<int>& numbers, int m, vector<int>& ans){
+void dfs(const vector<int>& numbers, int m, vector<int>& ans, int idx){
     if(ans.size() == m){
-        string str;
-        for(int i=0; i<ans.size(); i++)
-            str += to_string(ans[i]);
-        if(s.find(str) != s.end()) return;
-
-        s.insert(str);
-        for(int num : ans)
-            cout<<num<<" ";
-        cout<<"\n";
+        s.insert(ans);
         return;
     }
 
     for(int i=0; i<numbers.size(); i++){
-        if(idxCheck[i]) continue;
-        if(!ans.empty() && ans.back() > numbers[i]) continue;
-        
-        idxCheck[i] = true;
+        if(check[i]) continue;
+        if(!ans.empty() && ans.back()>numbers[i]) continue;
+
+        check[i] = true;
         ans.push_back(numbers[i]);
-
-        dfs(numbers,m,ans);
-
-        idxCheck[i] = false;
+        dfs(numbers,m,ans,i+1);
+        check[i] = false;
         ans.pop_back();
     }
 }
@@ -45,6 +35,12 @@ int main(){
         numbers.push_back(num);
     }
     sort(numbers.begin(), numbers.end());
-    dfs(numbers,m,ans);
+    dfs(numbers,m,ans,0);
+
+    for(vector<int> ans : s){
+        for(int num : ans)
+            cout<<num<<" ";
+        cout<<"\n";
+    }
     return 0;
 }
