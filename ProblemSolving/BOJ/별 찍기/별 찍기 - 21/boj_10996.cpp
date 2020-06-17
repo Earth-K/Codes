ba035556039fc817e800274dd7214bbea7f649fe
earth-k <k94earth@naver.com>
2020-06-17 23:18:32 +0900

    별 찍기 - 21 풀이 개선

--- ProblemSolving/BOJ/별 찍기/별 찍기 - 21/boj_10996.cpp
+++ ProblemSolving/BOJ/별 찍기/별 찍기 - 21/boj_10996.cpp
@@ -1,39 +1,17 @@
 #include<iostream>
 using namespace std;
-char map[2*101][101];
 int main(){
     int N;
     cin>>N;
-    if(N==1){
-        cout<<'*';
-        return 0;
-    }
-    
-    for(int i=0; i<2*N; i++){
-        for(int j=0; j<N; j++){
-            map[i][j] = ' ';
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=(N+1)/2 ;j++){
+            cout<<"* ";
         }
-    }
-
-    for(int i=0; i<2*N; i+=2){
-        int cnt=0;
-        int j=0;
-        while(cnt <= N){
-            if(cnt %2 == 0)
-                map[i][j] = '*';
-            else
-                map[i+1][j] = '*';
-            j += 1;
-            cnt += 1;
-        }
-    }
-
-    for(int i=0; i<2*N; i++){
-        for(int j=0; j<N; j++){
-            cout<<map[i][j];
+        cout<<"\n";
+        for(int j=1; j<=N/2 ;j++){
+            cout<<" *";
         }
         cout<<"\n";
     }
-
     return 0;
 }
